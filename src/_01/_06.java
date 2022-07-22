fac823373f419baa4471ab74ebd83908b26f3ecb
하경훈 <skyhoon8@gmail.com>
2022-07-22 10:24:51 +0900

    1-7.회문문자열

--- src/_01/_06.java
+++ src/_01/_06.java
@@ -9,13 +9,13 @@ public class _06 {
 		String answer="";
 		
 		for(int i=0; i<str.length();i++) {
-			//System.out.println(str.charAt(i)+" "+i+" "+str.indexOf(str.charAt(i)));
+			System.out.println(str.charAt(i)+" ||  str.charAt(i) : "+i+" ||  str.indexOf(str.charAt(i)) :  "+str.indexOf(str.charAt(i)));
 			if(str.indexOf(str.charAt(i))==i) {
 				answer+=str.charAt(i);
 			}
 		}
 		
-		/* 내가 푼 풀
+		/* 내가 푼 풀이
 		char[] s =  str.toCharArray();
 		
 		for(int i=0;i<s.length;i++) {
---
+++ src/_01/_07.java
@@ -0,0 +1,53 @@
+package _01;
+
+import java.util.*;
+
+public class _07 {
+
+	public String solution(String str) {
+		
+		/* 풀이 1
+		String answer = "YES";
+		str = str.toUpperCase();
+		int len = str.length();
+		for(int i=0;i<len/2;i++) {
+			if(str.charAt(i)!=str.charAt(len-i-1)) return "NO";
+		}
+		*/
+		
+		String answer = "NO";
+		String tmp = new StringBuilder(str).reverse().toString();
+		if(str.equalsIgnoreCase(tmp)) answer="YES";
+		
+		/* 내가 푼 풀이 (정답)
+		String answer = "";
+		String str2 = str.toUpperCase();
+		char[] s = str2.toCharArray();
+		int lt=0, rt= str2.length()-1;
+		
+		while(lt<rt) {
+			if(s[lt]==s[rt]) {
+				answer="YES";
+			}else {
+				answer="NO";
+				break;
+			}
+			lt++;
+			rt--;
+		}
+		*/
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		_07 T = new _07();
+		
+		Scanner kb = new Scanner(System.in);
+
+		String str = kb.next();
+			
+		System.out.println(T.solution(str));
+		
+	}
+
+}
