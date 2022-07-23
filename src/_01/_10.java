11b1e9224216446e6da60bf03fe3aaba83e4f0b2
하경훈 <skyhoon8@gmail.com>
2022-07-23 14:41:43 +0900

    #10

--- src/_01/_10.java
+++ src/_01/_10.java
@@ -4,21 +4,27 @@ import java.util.*;
 
 public class _10 {
 
-	public String solution(String str) {
+	public int[] solution(String str, char t) {
 	
-		String answer="";
-		
-		String[] sepStr = str.split(" ");
-		String s = sepStr[0];
-		String t = sepStr[1];
-		char[] r = s.toCharArray();
-		//System.out.print(s.indexOf(t));
-		String[] s2 = s.split(t);
-		
-		for(int i=0;i<s2.length;i++) {
-			System.out.print(s2[i]+i);
+		int[] answer = new int[str.length()];
+		int p =1000;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==t) {
+				p=0;
+				answer[i]=p;
+			} else {
+				p++;
+				answer[i]=p;
+			}
 		}
-		
+		p=1000;
+		for(int j=str.length()-1;j>=0;j--) {
+			if(str.charAt(j)==t) p=0;
+			else {
+				p++;
+				answer[j]=Math.min(answer[j], p);
+			}
+		}
 		return answer;
 		
 	}
@@ -27,9 +33,13 @@ public class _10 {
 		
 		Scanner kb = new Scanner(System.in);
 
-		String str = kb.nextLine();
-		System.out.println(T.solution(str));
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		for(int x : T.solution(str,c)) {
+			System.out.print(x+" ");
+		}
+		
 	
-	}
+	} 
 
 }
