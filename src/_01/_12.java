59191cb0ef93bbfc6db9994e89ca12def2702cbf
하경훈 <skyhoon8@gmail.com>
2022-07-24 16:11:38 +0900

    #12

--- src/_01/_12.java
+++ src/_01/_12.java
@@ -7,13 +7,29 @@ public class _12 {
 	public String solution(int n, String str) {
 	
 		String answer = "";
+		
+		//정답풀이
+		for(int i=0;i<n;i++) {
+			String tmp = str.substring(0,7).replace('#', '1').replace('*', '0');
+			int num = Integer.parseInt(tmp,2);
+			answer +=(char)num;
+			str=str.substring(7);
+		}
+		
+		/* 내가 푼 풀이
 		String[] newStr = new String[n];
 		for(int i=0;i<n;i++) {
 			newStr[i]=str.substring((i*7),(i*7)+7);
-			System.out.println(newStr[i]);
+			
+			newStr[i] = newStr[i].replaceAll("[#]", "1");
+			newStr[i] = newStr[i].replaceAll("[*]", "0");
+				
+			int binaryToDemical = Integer.parseInt(newStr[i],2);
+			
+			answer += String.valueOf((char)binaryToDemical);
 			
 		}
-		
+		*/
 		return answer;
 		
 	}
