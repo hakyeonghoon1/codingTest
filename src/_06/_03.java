851e2007f46b0facbbb298e4424093ccbf7fdc7c
하경훈 <skyhoon8@gmail.com>
2022-08-09 22:51:30 +0900

    #6-3

---
+++ src/_06/_03.java
@@ -0,0 +1,41 @@
+package _06;
+
+import java.util.*;
+
+public class _03 {
+
+	public int[] solution(int n, int[] arr) {
+		/**
+		 * 	선택정렬
+		 * 	
+		 */
+		for(int i=1;i<n;i++) {
+			int tmp = arr[i], j;
+			for(j=i-1;j>=0;j--) {
+				
+				if(arr[j]>tmp) arr[j+1]=arr[j];
+				else break;
+			}
+			arr[j+1]=tmp;
+		}
+		
+		return arr;
+		
+	}
+	public static void main(String[] args) {
+		_03 T = new _03();
+		
+		Scanner kb = new Scanner(System.in);
+
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) arr[i]=kb.nextInt();
+		for(int x: T.solution(n,arr)) {
+			System.out.print(x+" ");
+		}
+		
+	
+	} 
+	
+}
