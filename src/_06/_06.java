4b0e945a7ceee8020a6cb967bd797f6979677535
하경훈 <skyhoon8@gmail.com>
2022-08-14 16:57:06 +0900

    #6-6

---
+++ src/_06/_06.java
@@ -0,0 +1,35 @@
+package _06;
+
+import java.util.*;
+
+public class _06 {
+
+	public ArrayList<Integer> solution(int n, int[] arr) {
+		
+		ArrayList<Integer> answer = new ArrayList<>();
+		int[] tmp = arr.clone();
+		Arrays.sort(tmp);
+		for(int i=0;i<n;i++) {
+			if(arr[i]!=tmp[i]) answer.add(i+1);
+
+		}
+		
+		return answer;
+		
+	}
+	public static void main(String[] args) {
+		_06 T = new _06();
+		
+		Scanner kb = new Scanner(System.in);
+
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0;i<n;i++) arr[i]=kb.nextInt();
+		
+		for(int x : T.solution(n,arr))
+			System.out.print(x+" ");
+		
+	} 
+	
+}
