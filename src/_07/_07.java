dff689307575112a05f4e55b2d2d1c7f810c2909
하경훈 <skyhoon8@gmail.com>
2022-08-26 15:06:26 +0900

    #7-7 이진트리

---
+++ src/_07/_07.java
@@ -0,0 +1,48 @@
+package _07;
+
+import java.util.*;
+
+//class Node{
+//	int data;
+//	Node lt,rt;
+//	public Node(int val) {
+//		data=val;
+//		lt=rt=null;
+//	}
+//}
+
+public class _07 {
+	Node root;
+	public void BFS(Node root) {
+		/**
+		 *	이진트리 순회(BFS 넓이우선탐색:레벨탐색)
+		 */
+		
+		Queue<Node> Q = new LinkedList<>();
+		Q.offer(root);
+		int L=0;
+		while(!Q.isEmpty()) {
+			int len =Q.size();
+			System.out.print(L+" : ");
+			for(int i=0;i<len;i++) {
+				Node cur = Q.poll();
+				System.out.print(cur.data+" ");
+			}
+		}
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		_07 tree = new _07();
+		tree.root=new Node(1);
+		tree.root.lt=new Node(2);
+		tree.root.rt=new Node(3);
+		tree.root.lt.lt=new Node(4);
+		tree.root.lt.rt=new Node(5);
+		tree.root.rt.lt=new Node(6);
+		tree.root.rt.rt=new Node(7);
+		tree.BFS(tree.root);
+			
+	}
+}
