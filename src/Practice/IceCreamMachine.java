06d57f0cd60a74e4cd3b6d3772d0823b31ed272b
하경훈 <skyhoon8@gmail.com>
2022-09-08 15:07:03 +0900

    testdome

---
+++ src/Practice/IceCreamMachine.java
@@ -0,0 +1,45 @@
+package Practice;
+
+import java.util.List;
+
+public class IceCreamMachine {
+    public String[] ingredients;
+    public String[] toppings;
+    
+    public static class IceCream {
+        public String ingredient;
+        public String topping;
+
+        public IceCream(String ingredient, String topping) {
+            this.ingredient = ingredient;
+            this.topping = topping;
+        }
+    }
+
+    public IceCreamMachine(String[] ingredeints, String[] toppings) {
+        this.ingredients = ingredeints;
+        this.toppings = toppings;
+    }
+
+    public List<IceCream> scoops() {
+        throw new UnsupportedOperationException("Waiting to be implemented");
+    }
+
+    public static void main(String[] args) {
+        IceCreamMachine machine = new IceCreamMachine(new String[]{
+                "vanilla", "chocolate"
+        }, new String[]{
+                "chocolate sauce"
+        });
+        List<IceCream> scoops = machine.scoops();
+
+        /*
+         * Should print:
+         * vanilla, chocolate sauce
+         * chocolate, chocolate sauce
+         */
+        for (IceCream iceCream : scoops) {
+            System.out.println(iceCream.ingredient + ", " + iceCream.topping);
+        }
+    }
+}
---
+++ src/Practice/MergeNames.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MergeNames {
+    
+    public static String[] uniqueNames(String[] names1, String[] names2) {
+        Set<String> set = new HashSet<String>();
+        for(String x : names1) {
+        	set.add(x);
+        }
+        for(String y : names2) {
+        	set.add(y);
+        }
+        return set.toArray(new String[set.size()]);
+    }
+    
+    public static void main(String[] args) {
+        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
